3b97f8b10b7b12951e700d9c72a0e1e80dc49528
LoayAhmed304 <loayahmed304@gmail.com>
2025-02-22 22:10:48 +0200

    create http server to redirect non-secured routes to https

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const https = require("https");
+const http = require("http");
+
 const fs  = require("fs");
 
 const serverOptions = {
@@ -11,5 +13,10 @@ const httpsServer = https.createServer(serverOptions, (req, res) =>{
     res.end("Welcome to my localhost... EXCEPT ITS HTTPS!!!!!");
 });
 
+const httpServer = http.createServer((req, res)=>{
+    res.writeHead(301, {"Location":`https://localhost:8080`});
+    res.end();
+})
 
 httpsServer.listen(8080, ()=>console.log("Hello world, listening to server Loay"));
+httpServer.listen(80, ()=>console.log("HTTP running on port 80"));
