ff6ac324e09d11636f070a51801d83fa6f5a1f2a
LoayAhmed304 <loayahmed304@gmail.com>
2025-02-25 00:10:00 +0200

    add caching

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const https = require("https");
 const http = require("http");
 const httpProxy = require("http-proxy");
+const morgan = require("morgan");
+const NodeCache = require("node-cache");
 
 const sslConfig = require("./config/sslConfig");
 const proxyConfig = require("./config/proxyConfig");
@@ -8,7 +10,13 @@ const errorHandler = require("./utils/errorHandler");
 
 const proxy = httpProxy.createProxyServer();
 
-const httpsServer = https.createServer(sslConfig, (req, res) => {
+// Create cache
+const cache = new NodeCache({ stdTTL: 60, checkperiod: 120 }); // Cache expires in 60 seconds
+
+// Creating the HTTPS Server
+const httpsServer = https.createServer(sslConfig, (req, res, next) => {
+  morgan("combined")(req, res, () => {});
+
   const route = proxyConfig.find((route) => req.url.startsWith(route.path));
 
   if (!route) {
@@ -16,9 +24,34 @@ const httpsServer = https.createServer(sslConfig, (req, res) => {
     res.end("Bad Gateway: Undefined server requested");
     return;
   }
+
+  // Check if response is already cached -> then send the cached response
+  // Only for GET requests
+  if (req.method === "GET") {
+    const cachedResponse = cache.get(req.url);
+    if (cachedResponse) {
+      console.log("Serving from cache....", req.url);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(cachedResponse);
+      return;
+    }
+  }
+
+  // Proxy the request and cache the response
+  // Only cache GET requests
   proxy.web(req, res, { target: route.target }, (err) =>
     errorHandler(err, req, res)
   );
+  if (req.method === "GET") {
+    proxy.on("proxyRes", (proxyRes, req, res) => {
+      let body = "";
+
+      proxyRes.on("data", (chunk) => (body += chunk));
+      proxyRes.on("end", () => {
+        cache.set(req.url, body);
+      });
+    });
+  }
 });
 
 // Add custom header to all proxied requests
@@ -26,6 +59,7 @@ proxy.on("proxyReq", (proxyReq, req, res) => {
   proxyReq.setHeader("My-Header", "A custom header value made by the proxy");
 });
 
+// Handle proxy errors
 proxy.on("error", errorHandler);
 
 const httpServer = http.createServer((req, res) => {
